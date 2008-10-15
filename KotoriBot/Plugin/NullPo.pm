a8fb8cbe231a91e03cef6a072ff937e054905b22
UMEZAWA Takeshi <umezawa@utsoft.org>
2008-10-15 16:30:57 +0000

    「ぬるぽ」と同時に「ガッ」と発言している場合は、ガッしないようにした。

--- KotoriBot/Plugin/NullPo.pm
+++ KotoriBot/Plugin/NullPo.pm
@@ -15,7 +15,8 @@ sub on_public($$) {
 	my($self, $who, $message) = @_;
 	my $channel = $self->{channel};
 
-	if ($message =~ /(ぬ|ヌ|ﾇ)\s*(る|ル|ﾙ)\s*(ぽ|ポ|(ほ|ホ|ﾎ)\s*(゜|ﾟ))/) {
+	if ($message =~ /(ぬ|ヌ|ﾇ)\s*(る|ル|ﾙ)\s*(ぽ|ポ|(ほ|ホ|ﾎ)\s*(゜|ﾟ))/ &&
+			$message !~ /(が|ガ|(か|カ|ｶ)\s*(゛|ﾞ))\s*(っ|ッ|ｯ)/) {
 		$channel->notice("ガッ");
 	}
 }
