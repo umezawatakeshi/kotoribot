80e9c9d4e3c550a42a6bb5e986c492a9b3f6cc16
UMEZAWA Takeshi <umezawa@utsoft.org>
2008-12-19 20:40:02 +0000

    最初に入ってきた人にだけオペレータ権限を配布するプラグイン。

---
+++ KotoriBot/Plugin/OperDeal/OnlyFirst.pm
@@ -0,0 +1,27 @@
+# 文字コードはＵＴＦ－８、改行コードはＬＦのみ
+# $Id$
+
+package KotoriBot::Plugin::OperDeal::OnlyFirst;
+
+use strict;
+use warnings;
+use utf8;
+
+use KotoriBot::Plugin;
+use KotoriBot::Plugin::OperDeal;
+
+our @ISA = qw(KotoriBot::Plugin::OperDeal);
+
+sub on_join {
+	my($self, $who) = @_;
+	my $channel = $self->{channel};
+
+	my $nick = (split(/!/, $who))[0];
+	if (scalar($channel->nicks()) == 2) {
+		$channel->mode("+o", $nick);
+	}
+}
+
+###############################################################################
+
+return 1;
