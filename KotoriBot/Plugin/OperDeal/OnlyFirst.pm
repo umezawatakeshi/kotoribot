ff1b5d57c9573cb2c6600fc58f640ffe44b1c611
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-01-10 20:58:52 +0000

    - オペレータ権限を持っていない時は配布しようとしないようにした。 - 誰かが退出等した結果二人だけになった時も配布するようにした。

--- KotoriBot/Plugin/OperDeal/OnlyFirst.pm
+++ KotoriBot/Plugin/OperDeal/OnlyFirst.pm
@@ -16,9 +16,37 @@ sub on_join {
 	my $channel = $self->{channel};
 
 	my $nick = (split(/!/, $who))[0];
-	if (scalar($channel->nicks()) == 2) {
+	if (scalar($channel->nicks()) == 2 && $channel->am_operator()) {
 		$channel->mode("+o", $nick);
 	}
+}
+
+sub check_deal {
+	my($self) = @_;
+	my $channel = $self->{channel};
+
+	if (scalar($channel->nicks()) == 2 && $channel->am_operator()) {
+		my @nicks = grep { $_ ne $channel->server->irc->nick_name } $channel->nicks();
+		$channel->mode("+o", $nicks[0]);
+	}
+}
+
+sub on_part {
+	my($self) = @_;
+
+	$self->check_deal();
+}
+
+sub on_kick {
+	my($self) = @_;
+
+	$self->check_deal();
+}
+
+sub on_quit {
+	my($self) = @_;
+
+	$self->check_deal();
 }
 
 ###############################################################################
