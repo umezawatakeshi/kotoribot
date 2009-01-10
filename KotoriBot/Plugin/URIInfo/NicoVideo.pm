26cc07aaafc3923f94c45b6b4d64b694039be29f
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-01-10 21:25:13 +0000

    ニコニコ動画にログインする機能を追加した。 ログインしないで動作することも依然として可能である。

--- KotoriBot/Plugin/URIInfo/NicoVideo.pm
+++ KotoriBot/Plugin/URIInfo/NicoVideo.pm
@@ -8,30 +8,91 @@ use warnings;
 use utf8;
 
 use HTML::HeadParser;
+use HTTP::Request::Common;
 
 use KotoriBot::Plugin;
 
 our @ISA = qw(KotoriBot::Plugin);
 
+my $auth_mail = undef;
+my $auth_pass;
+
+my $wwwhostmatch = qr!http://www\.nicovideo\.jp/!;
+my $langhostmatch = qr!http://(tw|es|de)\.nicovideo\.jp/!;
+my $livehostmatch = qr!http://live\.nicovideo\.jp/!;
+
+my $watchmatch = qr!http://(?:www|tw|es|de)\.nicovideo\.jp/watch/..\d+!;
+my $livematch = qr!http://live\.nicovideo\.jp/watch/..\d+!;
+
+# パスワードファイルをチェック。
+# あまりイケてる方法ではないように思える。
+if (open(NICOPASS, "<nicopass.txt")) {
+	$auth_mail = <NICOPASS>; chomp($auth_mail);
+	$auth_pass = <NICOPASS>; chomp($auth_pass);
+	close(NICOPASS);
+}
+
 sub initialize {
 	my($self) = @_;
+	my $channel = $self->{channel};
 
-	my $uriinfo = $self->{channel}->plugin("KotoriBot::Plugin::URIInfo");
+	my $uriinfo = $channel->plugin("KotoriBot::Plugin::URIInfo");
 	if ($uriinfo) {
-		$uriinfo->add_output_plugin($self, qr!http://www\.nicovideo\.jp/watch/..\d+!, qr/.*/);
+		$uriinfo->add_output_plugin($self, qr!http://(?:www|live|tw|es|de)\.nicovideo\.jp/.*!, qr!(?:text|application)/x?html(?:\+xml)?!);
 	}
+
+	my $http = $channel->plugin("KotoriBot::Plugin::URIInfo::HTTP");
+	$self->{http} = $http;
+
+	my $html = $channel->plugin("KotoriBot::Plugin::URIInfo::HTML");
+	$self->{html} = $html;
 }
 
 sub output_content {
-	my($self, $context, $content, $ct) = @_;
+	my($self, $context, $content, $ct, $clen, $uri) = @_;
 
-	my $parser = HTML::HeadParser->new();
+	# ログインフォームが含まれるかどうか
+	if (defined($auth_mail) && $content =~ m!\<form [^<>]*action=\"https://secure.nicovideo.jp/secure/login!) {
+		$context->notice("ニコニコ動画にログインしています...");
 
-	$parser->parse($content);
-	$parser->eof();
+		my %addparam;
+		# なんで Referer を使わないんだろう…
+		if ($uri =~ $wwwhostmatch) {
+			$addparam{next_url} = "/$'";
+			$addparam{site} = "niconico";
+		} elsif ($uri =~ $langhostmatch) {
+			$addparam{next_url} = "/$'";
+			$addparam{site} = "$1niconico";
+		} elsif ($uri =~ $livehostmatch) {
+			$addparam{next_url} = "/$'";
+			$addparam{site} = "nicolive";
+		}
 
-	$context->notice_redirects();
-	$context->notice($parser->header("title") . " (" . scalar($parser->header("X-Meta-Keywords")) . ")");
+		my $req = HTTP::Request::Common::POST(
+			"https://secure.nicovideo.jp/secure/login",
+			{
+				mail => $auth_mail,
+				password => $auth_pass,
+				%addparam,
+			}
+		);
+		# 認証が成功したら同じ URL にリダイレクトされるので、ループ検出は無効にする。
+		$context->disable_loop_detection();
+		$self->{http}->do_request($context, $req);
+		return;
+	}
+
+	if ($uri =~ $watchmatch) {
+		my $parser = HTML::HeadParser->new();
+
+		$parser->parse($content);
+		$parser->eof();
+
+		$context->notice_redirects();
+		$context->notice($parser->header("title") . " (" . scalar($parser->header("X-Meta-Keywords")) . ")");
+	} else {
+		$self->{html}->output_content($context, $content, $ct, $clen, $uri);
+	}
 }
 
 ###############################################################################
