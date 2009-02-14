9a168c97204991b2f93dcdd55bb41cb8d59ac6e1
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-02-14 16:51:04 +0000

    ダイスの数が多すぎるときはエラーにする。

--- KotoriBot/Plugin/Dice.pm
+++ KotoriBot/Plugin/Dice.pm
@@ -11,6 +11,9 @@ use KotoriBot::Plugin;
 
 our @ISA = qw(KotoriBot::Plugin);
 
+# ちなみに dice は複数形で、
+# 単数形は die（米・英古）または dice（米略・英）であるらしい。
+
 sub on_public($$) {
 	my($self, $who, $message) = @_;
 	my $channel = $self->{channel};
@@ -23,18 +26,22 @@ sub on_public($$) {
 
 		next unless ($numdice > 0 && $faces > 0);
 
-		my $msg = "$numdice"."D$faces = ";
-		my $total = 0;
+		if ($numdice > 20) {
+			$channel->notice_error("Too many dice.");
+		} else {
+			my $msg = "$numdice"."D$faces = ";
+			my $total = 0;
 
-		for (my $i = 0; $i < $numdice; $i++) {
-			my $thisdice = int(rand($faces)) + 1;
-			$msg .= " + " if ($i != 0);
-			$msg .= $thisdice;
-			$total += $thisdice;
+			for (my $i = 0; $i < $numdice; $i++) {
+				my $thisdice = int(rand($faces)) + 1;
+				$msg .= " + " if ($i != 0);
+				$msg .= $thisdice;
+				$total += $thisdice;
+			}
+			$msg .= " = $total" if ($numdice > 1);
+
+			$channel->notice($msg);
 		}
-		$msg .= " = $total" if ($numdice > 1);
-
-		$channel->notice($msg);
 	}
 }
 
