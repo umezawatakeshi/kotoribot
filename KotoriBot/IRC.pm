71ccdf71bdcbb8dec4f5cd7c49dc1866818cef85
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-08-18 14:08:10 +0000

    改行を含むメッセージを送信しようとすると、 分割したコマンドとして扱われてしまう脆弱性を回避する。

--- KotoriBot/IRC.pm
+++ KotoriBot/IRC.pm
@@ -12,6 +12,14 @@ use POE qw(Component::IRC::State);
 
 our @ISA = qw(POE::Component::IRC::State);
 
+sub sl_prioritized {
+	$_[ARG1] =~ s/[\x00\x0a\x0d]/sprintf('\x{%04x}',ord($&))/eg;
+
+	my $self = shift;
+
+	return $self->SUPER::sl_prioritized(@_);
+}
+
 ###############################################################################
 
 return 1;
