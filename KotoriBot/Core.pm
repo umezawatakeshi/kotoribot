f49ddf75ee7207b7b9d9a58569a0ebe987545911
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-10-29 12:21:50 +0000

    バージョンを 1.13.0 とする。

--- KotoriBot/Core.pm
+++ KotoriBot/Core.pm
@@ -16,7 +16,7 @@ sub expand_serverhash($);
 
 my $instanceid = md5_hex(time() . " " . `uname -a`); # /dev/urandom も読むべきか。
 
-sub version { return "1.12.1"; }
+sub version { return "1.13.0"; }
 sub longversion { return "KotoriBot " . version(); }
 sub agent { return "KotoriBot/" . version(); }
 sub instanceid { return $instanceid; }
