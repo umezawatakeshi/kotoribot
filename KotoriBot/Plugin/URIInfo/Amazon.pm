f628399ac56a577947cf57a40f03d8d103f44593
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-11-10 14:51:41 +0000

    - 各国版 Amazon に関してのみ、max_size が 128KB になるようにする。 - asin: の後ろに空白文字を挟んでもハンドルするようにする。

--- KotoriBot/Plugin/URIInfo/Amazon.pm
+++ KotoriBot/Plugin/URIInfo/Amazon.pm
@@ -9,9 +9,20 @@ use utf8;
 
 use KotoriBot::Plugin;
 
-our @ISA = qw(KotoriBot::Plugin);
+our @ISA = qw(KotoriBot::Plugin::URIInfo::HTTP);
 
-my $match = qr/(asin):(\S+)/i;
+my $asinmatch = qr/(asin):\s*(\S+)/i;
+
+my $httpurlmatch = qr!https?://www\.amazon\..+?/[\#\%\&\(\)\*\+\,\-\.\/0-9\:\;\=\?\@A-Z\_a-z\~]*!;
+
+sub new {
+	my($class, $channel) = @_;
+
+	my $self = $class->SUPER::new($channel);
+	$self->{ua}->max_size(128 * 1024); # 128KB
+
+	return $self;
+}
 
 sub initialize {
 	my($self) = @_;
@@ -19,17 +30,20 @@ sub initialize {
 
 	my $uriinfo = $channel->plugin("KotoriBot::Plugin::URIInfo");
 	if ($uriinfo) {
-		$uriinfo->add_transform_plugin($self, $match);
+		$uriinfo->add_transform_plugin($self, $asinmatch);
+		$uriinfo->add_transform_plugin($self, $httpurlmatch);
 	}
 }
 
 sub transform_uri {
 	my($self, $context, $uri) = @_;
 
-	$uri =~ $match;
-
-	if ($1 eq "asin") {
-		$context->process_redirect("http://www.amazon.co.jp/dp/$2");
+	if ($uri =~ $asinmatch) {
+		if ($1 eq "asin") {
+			$context->process_redirect("http://www.amazon.co.jp/dp/$2");
+		}
+	} elsif ($uri =~ $httpurlmatch) {
+		$self->SUPER::transform_uri($context, $uri);
 	}
 }
 
