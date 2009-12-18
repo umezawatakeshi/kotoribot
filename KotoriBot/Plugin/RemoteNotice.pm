d4cc22ff88e9e224808329336f934337c31feb7e
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-12-18 14:24:35 +0000

    他のチャンネルに notice するためのプラグイン。

---
+++ KotoriBot/Plugin/RemoteNotice.pm
@@ -0,0 +1,47 @@
+# 文字コードはＵＴＦ－８、改行コードはＬＦのみ
+# $Id$
+
+package KotoriBot::Plugin::RemoteNotice;
+
+use strict;
+use warnings;
+use utf8;
+
+use KotoriBot::Plugin;
+
+our @ISA = qw(KotoriBot::Plugin);
+
+sub on_public($$) {
+	my($self, $who, $message) = @_;
+	my $channel = $self->{channel};
+
+	if ($message =~ /^\\\\([^\\]+)(.*)/) {
+		my $nick = $1;
+		my $cmd = $2;
+		return if $nick ne $channel->server()->irc()->nick_name();
+		$message = $cmd;
+	}
+
+	if ($message =~ /^\\remotenotice +(\#\S+) +(.+)/) {
+		my $rchannelname = $1;
+		my $rmsg = $2;
+
+		my $rchannel = $channel->server()->channel($rchannelname);
+		if (defined($rchannel)) {
+			$rchannel->notice($rmsg);
+		} else {
+			$channel->notice_error("no such channel");
+		}
+	} elsif ($message =~ /^\\globalnotice +(.+)/){
+		my $rmsg = $1;
+
+		my @channels = $channel->server()->channels();
+		foreach my $rchannel (@channels) {
+			$rchannel->notice($rmsg) if ($rchannel != $channel);
+		}
+	}
+}
+
+###############################################################################
+
+return 1;
